01b6cbe2da8448da3bb14a33a3251d262cb714ba
Ricardo Flores <rik.fm1809@gmail.com>
2021-05-10 21:24:02 -0500

    estructura html, fondos y efecto glass

--- pages/index.js
+++ pages/index.js
@@ -3,63 +3,51 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
-        <title>Create Next App</title>
+        <title>Ricardo Flores - Portfolio</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+          <section className={`${styles.container_profile} ${styles.glass} ${styles.mb_5}`}>
+            <div className={styles.photo}>
+              <img src="/assets/imgs/avatar.jpg" alt="" className={ `${styles.img__avatar}` }/>
+              <h2>Ricardo Flores</h2>
+            </div>
+            <ul className={styles.links}>
+              <li className={styles.link}>
+                <a href="https://twitter.com/Ricardo9Fm" target="_blank" rel="noopener noreferrer">
+                  <img className={styles.redes} src="/assets/imgs/twitter.svg" alt=""/>
+                  @Ricardo9Fm
+                </a>
+              </li>
+              <li className={styles.link}><a href="https://github.com/lemcok" target="_blank" rel="noopener noreferrer"><img src="/assets/imgs/github.svg" alt=""/>lemcok</a></li>
+              <li className={styles.link}><a href="https://www.linkedin.com/in/ricardo-flores-morales/" target="_blank" rel="noopener noreferrer"><img src="/assets/imgs/linkedin.svg" alt=""/>Linkedin</a></li>
+              <li className={styles.link}><a href="" style={{'text-decoration': 'underline', 'color': 'blue'}}><img src="/assets/imgs/cv.svg" alt=""/>CV</a></li>
+            </ul>
+          </section>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+          <section className={`${styles.glass} ${styles.container_cards}`}>
+            <div className={ `${styles.card} ${styles.glass}`}>
+              <img src="/assets/imgs/task-app.png" alt="" className={styles.card_img}/>
+              <div className={styles.card_body}>
+                <h2>Task-App</h2>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+              </div>
+            </div>
+            <div className={ `${styles.card} ${styles.glass}`}>
+              <img src="/assets/imgs/task-app.png" alt="" className={styles.card_img}/>
+              <div className={styles.card_body}>
+                <h2>Task-App</h2>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+              </div>
+            </div>
+          </section>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+      <div className={styles.circle1}></div>
+      <div className={styles.circle1}></div>
+    </>
   )
 }
